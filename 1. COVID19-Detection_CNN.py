5e3c98ad969f1d66fffadeed3e70c081d1d2d6ff
Mohammed Lajam <88610375+MohammedLajam@users.noreply.github.com>
2022-06-28 09:51:29 +0200

    dataset explanation

---
+++ 1. COVID19-Detection_CNN.py
@@ -0,0 +1,8 @@
+'''
+Dataset: there are 2 files for train and test dataset (Images). these files have only
+image file names and it does not show the class (Positive or Negative) of the image.
+additionally, there are 2 extra txt files for train and test sets showing the Patient ID, filename,
+class (Positive or Negative) and data source. By using those files, we can link which images have
+positive and which have negative (for both train and test datasets)
+'''
+
