f677fbc9d55bc05e5d660935744b3523fcac29a2
Mohammed Lajam <88610375+MohammedLajam@users.noreply.github.com>
2022-06-30 19:43:48 +0200

    building a CNN model

--- 2. COOVID19-Detection_CNN2.py
+++ 2. COOVID19-Detection_CNN2.py
@@ -19,11 +19,10 @@ import matplotlib.pyplot as plt
 import numpy as np
 from keras.models import Sequential
 from keras.layers import Conv2D, Flatten, MaxPool2D, Dense, Dropout
+from sklearn.model_selection import train_test_split
 import os
 import PIL
 import cv2
-
-import tensorflow as tf
 from tensorflow import keras
 
 
@@ -50,6 +49,8 @@ test_df = test_df.drop(['patient id', 'data source'], axis=1)
 # first, we need to classify the dataset (which are positive and which are negative)
 x = train_df.drop(['class'], axis=1)
 y = train_df['class']
+x = train_df.drop(['class'], axis=1)
+y = train_df['class']
 print(y.value_counts())  # before resampling
 
 rus = RandomUnderSampler(sampling_strategy=1)
@@ -60,6 +61,14 @@ train_df = shuffle(train_df)  # shuffling the dataset
 print(train_df['class'].value_counts())  # after resampling
 print(train_df.head())
 
+# splitting the training set into train and validation data:
+train_df, valid_df = train_test_split(train_df, train_size=0.9, random_state=0)
+print('train, validation and test sets:')
+print(f"Negative and positive values of train: {train_df['class'].value_counts()}")
+print(f"Negative and positive values of validation: {valid_df['class'].value_counts()}")
+print(f"Negative and positive values of test: {test_df['class'].value_counts()}")
+
+# Directory iterator:
 # using flow_from_dataframe function to link the class of the txt file to the image folder
 train_datagen = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input, rescale=1.0/255.0, validation_split=0.2)
 test_datagen = ImageDataGenerator(preprocessing_function=tf.keras.applications.vgg16.preprocess_input, rescale=1.0/255.0, validation_split=0.2)
@@ -67,12 +76,16 @@ test_datagen = ImageDataGenerator(preprocessing_function=tf.keras.applications.v
 # preparing the data in a format that the model expects using flow_from_dataframe:
 train_gen = train_datagen.flow_from_dataframe(dataframe=train_df, directory=train_path, x_col='filename',
                                               y_col='class', classes=['positive', 'negative'],
-                                              target_size=(200, 200), batch_size=64, class_mode='binary')
+                                              target_size=(200, 200), batch_size=100, class_mode='binary')
+valid_gen = test_datagen.flow_from_dataframe(dataframe=valid_df, directory=train_path, x_col='filename',
+                                             y_col='class', classes=['positive', 'negative'],
+                                             target_size=(200, 200), batch_size=100, class_mode='binary')
 test_gen = test_datagen.flow_from_dataframe(dataframe=test_df, directory=test_path, x_col='filename',
                                             y_col='class', classes=['positive', 'negative'],
-                                            target_size=(200, 200), batch_size=64, class_mode='binary', shuffle=False)
+                                            target_size=(200, 200), batch_size=100, class_mode='binary', shuffle=False)
 
 x_train, y_train = next(train_gen)
+x_valid, y_valid = next(valid_gen)
 x_test, y_test = next(test_gen)
 print(x_train.shape)
 print(y_train.shape)
@@ -80,9 +93,13 @@ print(x_test.shape)
 print(y_test.shape)
 
 # plotting an image as an example:
-plt.imshow(x_train[10])
+plt.imshow(x_train[10])  # plotting one image from the train set
 plt.show()
 print(y_train[10])
+plt.imshow(x_test[10])  # plotting one image from the test set
+plt.show()
+print(y_test[10])
+
 '''
 def Plot_images(self, imges_arr):
     fig, axes = plt.subplot(1, 10, figsize=(20, 20))
@@ -93,4 +110,24 @@ def Plot_images(self, imges_arr):
     plt.tight_layout()
     plt.show()
 '''
+
+# 2. Building a Model (CNN):
+model = Sequential([Conv2D(filters=32, kernel_size=(3, 3), activation='relu', padding='same', input_shape=(200, 200, 3)),
+                    MaxPool2D(pool_size=(2, 2), strides=2),
+                    Conv2D(filters=64, kernel_size=(3, 3), activation='relu', padding='same'),
+                    MaxPool2D(pool_size=(2, 2), strides=2),
+                    Flatten(),
+                    Dense(units=2, activation='softmax'),
+                    ])
+model.summary()
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+# In this case during fitting the model, we mention only the x parameter (y is not included)
+# because the train_gen is generated from a function ImageDataGenerator, in which the labels
+# are integrated along side with the images.
+model.fit(x=train_gen, validation_data=valid_gen, epochs=5, steps_per_epoch=250, verbose=2)
+
+# predictions:
+predictions = model.predict(x=test_gen, verbose=0)
+# printing the result in a shape of an array with 1 is the higher probable class
+print(np.round(predictions))
 
